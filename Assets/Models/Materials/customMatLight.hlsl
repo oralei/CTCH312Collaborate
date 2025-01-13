f86632821fc0203915161afd521ef87af7e7b685
oralei <73796709+oralei@users.noreply.github.com>
2025-01-13 16:04:02 -0600

    toon shader + textures

---
+++ Assets/Models/Materials/customMatLight.hlsl
@@ -0,0 +1,8 @@
+#if defined(SHADERGRAPH_PREVIEW)
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+#else
+    Light light = GetMainLight();
+    Direction = light.direction;
+    Color = light.color;
+#endif
